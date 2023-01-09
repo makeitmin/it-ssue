804921454d1b8fb1c0738bcb6635983ee9710927
makeitmin <seongminha.dev@gmail.com>
2023-01-09 16:07:01 +0900

    ✨FEAT: Alert 추가
    초과 등록시 알림 기능

--- src/components/features/BookmarkButton.jsx
+++ src/components/features/BookmarkButton.jsx
@@ -8,8 +8,11 @@ import styled from 'styled-components';
 import useBookmarkStore from '../../store/useBookmarkStore';
 import { getBookmarks } from '../../utils';
 
+import WarningAlert from './WarningAlert';
+
 const BookmarkButton = ({ data }) => {
   const [isChecked, setIsChecked] = useState(false);
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
   const { setUserBookmarks } = useBookmarkStore(state => state);
 
   useEffect(() => {
@@ -22,6 +25,11 @@ const BookmarkButton = ({ data }) => {
   const handleChangeBookmark = event => {
     let temp = getBookmarks();
     if (event.target.checked) {
+      if (temp.length >= 4) {
+        setIsAlertOpen(true);
+        event.target.checked = false;
+        return false;
+      }
       temp.push({
         id: data.id,
         full_name: data.full_name,
@@ -35,13 +43,20 @@ const BookmarkButton = ({ data }) => {
   };
 
   return (
-    <BookmarkCheckbox
-      checked={isChecked}
-      onChange={handleChangeBookmark}
-      size="small"
-      icon={<BookmarkBorderIcon />}
-      checkedIcon={<BookmarkIcon />}
-    />
+    <>
+      <BookmarkCheckbox
+        checked={isChecked}
+        onChange={handleChangeBookmark}
+        size="small"
+        icon={<BookmarkBorderIcon />}
+        checkedIcon={<BookmarkIcon />}
+      />
+      <WarningAlert
+        open={isAlertOpen}
+        setOpen={setIsAlertOpen}
+        message="북마크는 4개를 초과할 수 없습니다"
+      />
+    </>
   );
 };
 
