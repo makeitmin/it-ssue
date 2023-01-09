e7d16362b205caf6fe6f88abc304340dd9fdfba9
makeitmin <seongminha.dev@gmail.com>
2023-01-09 16:48:58 +0900

    🐛FIX: 북마크 오류 수정
    기존 북마크 호출 시 null 값을 반환하는 오류

--- src/components/features/BookmarkButton.jsx
+++ src/components/features/BookmarkButton.jsx
@@ -11,21 +11,26 @@ import { getBookmarks } from '../../utils';
 import WarningAlert from './WarningAlert';
 
 const BookmarkButton = ({ data }) => {
-  const [isChecked, setIsChecked] = useState(false);
-  const [isAlertOpen, setIsAlertOpen] = useState(false);
-  const { setUserBookmarks } = useBookmarkStore(state => state);
+  const [isChecked, setIsChecked] = useState(false); // 북마크 체크 여부
+  const [isAlertOpen, setIsAlertOpen] = useState(false); // 개수 초과 Alert 노출 여부
+
+  /* 북마크 배열 변수 (전역) */
+  const { userBookmarks, setUserBookmarks } = useBookmarkStore(state => state);
 
   useEffect(() => {
-    const bookmarks = getBookmarks();
-    if (bookmarks.find(b => b.id === data.id)) {
+    /* 기존 북마크 배열과 비교하여 해당 Repository의 북마크 여부 확인 */
+    if (userBookmarks.find(b => b.id === data.id)) {
       setIsChecked(true);
     }
   });
 
   const handleChangeBookmark = event => {
     let temp = getBookmarks();
+
     if (event.target.checked) {
+      /* 북마크 X -> 북마크 O 일 경우 */
       if (temp.length >= 4) {
+        /* 기존 북마크 4개 초과시 */
         setIsAlertOpen(true);
         event.target.checked = false;
         return false;
@@ -36,6 +41,7 @@ const BookmarkButton = ({ data }) => {
         topics: data.topics,
       });
     } else {
+      /* 북마크 X -> 북마크 O 일 경우 */
       temp = temp.filter((t, idx) => t.id !== data.id);
     }
     setUserBookmarks(temp);
